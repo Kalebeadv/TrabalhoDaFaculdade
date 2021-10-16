d2e2f8c89cd0d21ac7899a21878ff2a3b9d259dd
Kalebe adv <kalebeadv66@gmail.com>
2021-10-16 18:52:40 -0300

    adicionando outras classes base

---
+++ src/condominio/Jair/parte2/Visitante.java
@@ -0,0 +1,17 @@
+package condominio.Jair.parte2;
+
+public class Visitante {
+	private String funcao;
+	private String nome;
+	
+	
+	public String getFuncao() {
+		return funcao;
+	}
+	public String getNome() {
+		return nome;
+	}
+	
+	
+
+}
